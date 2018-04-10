40d15621d8d7c0e00ea1901fbf99f60bcd3faf5a
sowmith8899 <38257906+sowmith8899@users.noreply.github.com>
2018-04-10 23:46:51 +0530

    Update K1620_A17.C

--- K1620_A17.C
+++ K1620_A17.C
@@ -1,12 +1,11 @@
 #include<stdio.h>
-
 main()
 {
 	int timer=0,i=0,bt=0;
 	int process[4]={1,2,3,4};
-	int arrival[4]={0,5,13,17};
-	int burst_time[4]={20,36,19,42};
-	int burst[4]={20,36,19,42};
+	int arrival[4]={0,2,4,5};
+	int burst_time[4]={7,4,1,4};
+	int burst[4]={7,4,1,4};
 	int process_arrived[4]={1};
 	double priority[4]={0};
 	int complete[4]={0};
